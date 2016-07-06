6b7a7b78bdc310e493573a58ba63bdce6f07638d
Justus Adam <dev@justus.science>
2016-07-06 18:30:46 +0200

    Adding a proper condition

--- src/Backends/Haskell.hs
+++ src/Backends/Haskell.hs
@@ -24,7 +24,7 @@ cgNodeToHaskellFunction children (n,CodeGraphNodeLabel _ ctype t) =
         Map -> "map ifn" ++ nodeToUniqueName n ++ " " ++ timeoutChildrenList
         SideEffect -> "writeData \"service-name\" " ++ timeoutChildrenList
         Conditional cond trueBranch falseBranch ->
-            "if " ++ maybe "True" nodeToUniqueName cond ++ " then return " ++ f trueBranch ++ " else return " ++ f falseBranch
+            "return $ if " ++ maybe "True" ( (++ " == 0") . nodeToUniqueName) cond ++ " then " ++ f trueBranch ++ " else " ++ f falseBranch
           where f = maybe "0" nodeToUniqueName
         Rename name -> "return " ++ name
   where
