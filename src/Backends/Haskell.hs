a8a161f963d5f4f1034c249a5a15250e838750bb
Justus Adam <dev@justus.science>
2016-07-06 18:29:02 +0200

    Trying a different way

--- src/Backends/Haskell.hs
+++ src/Backends/Haskell.hs
@@ -24,7 +24,7 @@ cgNodeToHaskellFunction children (n,CodeGraphNodeLabel _ ctype t) =
         Map -> "map ifn" ++ nodeToUniqueName n ++ " " ++ timeoutChildrenList
         SideEffect -> "writeData \"service-name\" " ++ timeoutChildrenList
         Conditional cond trueBranch falseBranch ->
-            "return $ if " ++ maybe "True" nodeToUniqueName cond ++ " then " ++ f trueBranch ++ " else " ++ f falseBranch
+            "if " ++ maybe "True" nodeToUniqueName cond ++ " then return " ++ f trueBranch ++ " else return " ++ f falseBranch
           where f = maybe "0" nodeToUniqueName
         Rename name -> "return " ++ name
   where
